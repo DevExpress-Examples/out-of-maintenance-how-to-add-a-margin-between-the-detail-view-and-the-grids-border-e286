1e43f0f62877732359ed713a7625c8f576aba5dc
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-11 17:06:29 +0400

    Change example status to out of maintenance

--- VB/detailViewMarginVB/Form1.vb
+++
@@ -1,8 +0,0 @@
-Public Class Form1
-
-    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        'TODO: This line of code loads data into the 'NwindDataSet.Orders' table. You can move, or remove it, as needed.
-        Me.OrdersTableAdapter.Fill(Me.NwindDataSet.Orders)
-        Me.Order_DetailsTableAdapter1.Fill(Me.NwindDataSet.Order_Details)
-    End Sub
-End Class
